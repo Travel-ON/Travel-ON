7224ce1d227738413458a840748547d9419e54c3
JHPark <erict4f@gmail.com>
2022-07-26 20:52:52 +0900

    jaehyun_Test: 본문 좌우 스페이싱 뷰파일 생성

---
+++ frontend/MainSpacing.vue
@@ -0,0 +1,22 @@
+<template>
+  <div style="display: flex; justify-content: space-between">
+    <div class="left-space"></div>
+    <div class="main-space">
+      <h1>This is main space</h1>
+    </div>
+    <div class="right-space"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainSpacing",
+};
+</script>
+
+<style>
+.main-space {
+  width: 1128px;
+  background-color: skyblue;
+}
+</style>
