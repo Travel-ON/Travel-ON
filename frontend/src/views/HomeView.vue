1fba9de28763a17ec87ad727de71439e301cf999
erict4f <erict4f@gmail.com>
2022-08-01 17:45:34 +0900

    jaehyun_Feat: MainNav 기능 추가

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,20 @@
 <template>
-  <hello-world />
+  <MainNav />
+  <div style="margin-top: 80px">
+    <img src="https://via.placeholder.com/1920x1080" alt="main-banner" style="width: 100%" />
+  </div>
+  <MainSpacingHome />
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+import MainNav from "@/components/MainNav.vue";
+import MainSpacingHome from "@/components/MainSpacingHome.vue";
+import { defineComponent } from "vue";
 
-export default {
-  name: "Home",
+// Components
+
+export default defineComponent({
+  name: "HomeView;",
 
   components: {
     MainNav,
