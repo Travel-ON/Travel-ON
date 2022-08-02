e6c1b4630618c3871ec211b6fe4407df21719127
Kuuuna98 <yunaghgh@naver.com>
2022-08-02 16:41:20 +0900

    yuna_Feat: 신규알림 여부 확인 기능 추가

--- backend/travel-on/src/main/java/com/travel/travel_on/controller/UserController.java
+++ backend/travel-on/src/main/java/com/travel/travel_on/controller/UserController.java
@@ -274,6 +274,21 @@ public class UserController {
         }
     }
 
+    @ApiOperation(value = "신규알림 여부 조회: 신규알림이 생성됐는지 여부를 확인한다", response = Boolean.class)
+    @GetMapping("/alarm/{id}")
+    public ResponseEntity<?> alarmCheck(@PathVariable String id) {
+        try {
+            UserDto userDto = usvc.select(id);
+            if(userDto==null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<Boolean>(userDto.isAlarmFlag(), HttpStatus.OK);
+        } catch (Exception e) {
+            return exceptionHandling(e);
+        }
+    }
+
+
     private ResponseEntity<String> exceptionHandling(Exception e) {
         e.printStackTrace();
         return new ResponseEntity<String>("Sorry: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
