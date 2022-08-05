bf5a07b408aa337c27c05e3760ec908e80b09736
t21212 <t21212@naver.com>
2022-08-05 21:08:05 +0900

    taehun_Modify: 찐막 수정

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -13,11 +13,7 @@ import axios from "axios";
 
 function createApi() {
   return axios.create({
-<<<<<<< HEAD
-    baseURL: `http://localhost:8080/`,
-=======
     baseURL: `http://i7b301.p.ssafy.io:3000/api`,
->>>>>>> 64b23c97df8aa894722af673e62c130e930371ae
     headers: {
       "Content-Type": "application/json",
     },
