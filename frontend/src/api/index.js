048b63915127c91b73c3a059d49f62d10c412ecb
Taehun <t21212@naver.com>
2022-08-05 20:34:35 +0900

    taehun_Modify: notice 수정

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -13,7 +13,7 @@ import axios from "axios";
 
 function createApi() {
   return axios.create({
-    baseURL: `https://7d0c97ac-6236-4b8f-9ba8-f257edcdfe2d.mock.pstmn.io//`,
+    baseURL: `http://localhost:8080/`,
     headers: {
       "Content-Type": "application/json",
     },
