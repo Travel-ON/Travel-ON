6cc6b02472cebb7b643dd1d3201de4516b6b3e96
Kuuuna98 <yunaghgh@naver.com>
2022-08-05 17:23:24 +0900

    yuna_Feat: 회원관리 일부 기능에 JWT 적용함

--- backend/travel-on/src/main/java/com/travel/travel_on/config/SecurityConfig.java
+++ backend/travel-on/src/main/java/com/travel/travel_on/config/SecurityConfig.java
@@ -1,17 +1,24 @@
 package com.travel.travel_on.config;
 
+import com.travel.travel_on.auth.JwtAuthenticationFilter;
+import com.travel.travel_on.auth.JwtUserDetailService;
+import com.travel.travel_on.model.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
-@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] PERMIT_ALL_URL_ARRAY = {
@@ -31,51 +38,44 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/notice/detail/**",
     };
 
-    private static final String[] PERMIT_ADMIN_URL_ARRAY = {
-            // controller
-            "/qna/admin/**",
-            "/notice/**",
-    };
+    @Autowired
+    private JwtUserDetailService jwtUserDetailService;
 
-    private static final String[] PERMIT_ADMIN_AND_USER_URL_ARRAY = {
-            // controller
-            "/qna/detail/**",
-            "/qna/modify",
-    };
+    @Autowired
+    private UserService userService;
 
-    private static final String[] PERMIT_USER_URL_ARRAY = {
-            // controller
-            "/qna/**",
-            "/user/**",
-            "/alarm/**",
-            "/plan/**",
-            "/videochat/**",
-    };
-
-
-
-
-    @Override
-    public void configure(WebSecurity web) {
-//        web.ignoring()
-//                .antMatchers(
-//                        "/favicon.ico"
-//                        ,"/error"
-//                        ,"/swagger-ui/**"
-//                        ,"/swagger-resources/**"
-//                        ,"/v3/api-docs/**"
-//                );
+    // Password 인코딩 방식에 BCrypt 암호화 방식 사용
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
+
+    // DAO 기반으로 Authentication Provider를 생성
+    // BCrypt Password Encoder와 UserDetailService 구현체를 설정
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(this.jwtUserDetailService);
+        return daoAuthenticationProvider;
+    }
+
+    // DAO 기반의 Authentication Provider가 적용되도록 설정
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable() // swagger API 호출시 403 에러 발생 방지
-                .authorizeRequests()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 역시 사용하지 않습니다.
+                .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager(), userService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
+                .authorizeRequests() // 요청에 대한 사용권한 체크
                 .antMatchers(PERMIT_ALL_URL_ARRAY).permitAll()
-                .antMatchers(PERMIT_ADMIN_URL_ARRAY).access("hasRole('ADMIN')") // ADMIN만 가능
-                .antMatchers(PERMIT_ADMIN_AND_USER_URL_ARRAY).access("hasRole('ADMIN') or hasRole('USER')") //ADMIN or SYS
-                .antMatchers(PERMIT_USER_URL_ARRAY).hasRole("USER") // 페이지 유저만 가능
                 .anyRequest().authenticated();
     }
 
