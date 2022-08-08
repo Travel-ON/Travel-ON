b590dc6ad78223824c8e8703f3adb141165c128c
kooyeon <guyontop@naver.com>
2022-08-08 17:51:39 +0900

    kooyeon_Feat: 플래너 작성

---
+++ frontend/src/components/planner/PlannerStory.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="PlannerStory">
+    <h1>플래너스토리</h1>
+    <ModalView v-if="isModalViewed" @close-modal="isModalViewed = false"> </ModalView>
+    <button @click="isModalViewed = true">스토리보기</button>
+  </div>
+</template>
+
+<script>
+// import ModalView from "./components/planner/ModalView";
+
+export default {
+  name: "PlannerStory",
+  components: {},
+  data() {
+    return {
+      isModalViewed: false,
+    };
+  },
+};
+</script>
