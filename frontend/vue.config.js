3312c7d74f85c661df6a1377df29bdf9972be64c
t21212 <t21212@naver.com>
2022-08-09 21:53:48 +0900

    taehun_Modify: vue.config.js / index.html 배포용 수정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,12 +3,17 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   
-  devServer: {
-         https: false,
+    devServer: {
+         host: '0.0.0.0',
          port: 8080,
          open: true,
          allowedHosts:'all',
-         
+         client: {
+            webSocketURL: 'wss://i7b301.p.ssafy.io:443/ws',         
+         },
+         headers: {
+            'Access-Control-Allow-Origin' : '*',
+         }
   },
 
   pluginOptions: {
