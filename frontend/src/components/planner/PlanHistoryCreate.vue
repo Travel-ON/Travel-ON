9ab42807f53d75646e7964c134231e75626e2205
JHPark <erict4f@gmail.com>
2022-08-11 03:16:52 +0900

    jaehyun_Feat: push before offline

---
+++ frontend/src/components/planner/PlanHistoryCreate.vue
@@ -0,0 +1,80 @@
+<template>
+  <div style="flex: 6">
+    <div style="height: 90px; display: flex; align-items: center">
+      <div style="display: flex; align-items: center; margin-left: 20px">
+        <div style="margin-right: 10px"><v-icon>mdi-plus-circle-outline</v-icon></div>
+        <div style="font-weight: bold; font-size: 28px">방문장소 추가</div>
+      </div>
+    </div>
+    <div style="display: flex; justify-content: center; margin: 0 50px">
+      <v-form>
+        <div
+          style="
+            width: 560px;
+            padding: 20px 50px 0px;
+            background-color: #c5dcff;
+            border-radius: 12px;
+            margin-bottom: 30px;
+          "
+        >
+          <div style="display: flex; align-items: center; margin-bottom: 10px">
+            <div><v-icon>mdi-pen</v-icon></div>
+            <div style="font-size: 16px; margin-left: 10px; font-weight: bold">장소 작성 (필수)</div>
+          </div>
+          <div style="text-align: left; margin-left: 30px">
+            <label for="planDate">
+              <input
+                type="date"
+                id="planDate"
+                v-model="date"
+                style="border-bottom: 2px solid #adadad; margin-bottom: 30px; margin-right: auto"
+              />
+            </label>
+          </div>
+          <div style="margin: 0 30px; display: flex">
+            <v-select label="시(도)" style="margin-right: 30px" bg-color="#efefef" density="comfortable"></v-select>
+            <v-select label="구(군)" bg-color="#efefef" density="comfortable"></v-select>
+          </div>
+          <v-text-field
+            label="장소를 입력해주세요"
+            prepend-inner-icon="mdi-map-marker"
+            bg-color="#efefef"
+            style="margin: 0 30px"
+            density="comfortable"
+          ></v-text-field>
+        </div>
+        <div>
+          <div
+            style="
+              width: 560px;
+              padding: 20px 50px;
+              background-color: #c5dcff;
+              border-radius: 12px;
+              margin-bottom: 30px;
+            "
+          >
+            <div style="display: flex; align-items: center; margin-bottom: 30px">
+              <div><v-icon>mdi-pen</v-icon></div>
+              <div style="font-size: 16px; margin-left: 10px; font-weight: bold">리뷰 작성 (선택)</div>
+              <div style="display: flex">
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </v-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    date: new Date().toISOString().substr(0, 10),
+    dateMenu: false,
+  }),
+};
+</script>
+
+<style></style>
--- frontend/src/views/PlannerView.vue
+++ frontend/src/views/PlannerView.vue
@@ -2,22 +2,48 @@
   <div style="display: flex; justify-content: space-between">
     <div class="left-space"></div>
     <div class="main-space">
-      <div style="position: absolute; right: -20px; top: 40px">
+      <div
+        :style="`position: absolute; right: -20px; top: ${40 + 50 * item}px`"
+        v-for="item in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]"
+        :key="item"
+      >
         <v-img
           src="https://user-images.githubusercontent.com/97648026/183853221-9475cb02-66dc-4b69-abef-dc280908b172.png"
           height="30"
           width="50"
         ></v-img>
       </div>
-      <h1>This is main space</h1>
+      <div v-if="selectedPage === 0">
+        <div
+          style="
+            height: 80px;
+            width: 100%;
+            border-bottom: 2px solid #adadad;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          "
+        >
+          <div><h1>방문한 장소</h1></div>
+        </div>
+        <div class="plan-content">
+          <PlanHistoryCreate />
+        </div>
+      </div>
     </div>
     <div class="right-space"></div>
   </div>
 </template>
 
 <script>
+import PlanHistoryCreate from "@/components/planner/PlanHistoryCreate.vue";
+
 export default {
   name: "PlannerView",
+  data: () => ({
+    selectedPage: 0,
+  }),
+  components: { PlanHistoryCreate },
 };
 </script>
 
@@ -25,8 +51,13 @@ export default {
 .main-space {
   width: 1128px;
   height: 966px;
-  padding: 30px;
+  padding: 30px 50px 30px 30px;
   background-color: #fff4e4;
   position: relative;
+  border: 2px solid #adadad;
+}
+.plan-content {
+  display: flex;
+  height: 100%;
 }
 </style>
