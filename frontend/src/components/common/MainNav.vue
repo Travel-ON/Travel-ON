b8f250b31949c5f0d3eb38d45221ef88d3e895f4
t21212 <t21212@naver.com>
2022-08-12 17:32:32 +0900

    taehun_Feat: FE - 칭호설정 페이지 구현

--- frontend/src/components/common/MainNav.vue
+++ frontend/src/components/common/MainNav.vue
@@ -13,9 +13,15 @@
       <router-link :to="{ name: 'VideochatCreate' }">
         <v-btn>방만들기</v-btn>
       </router-link>
-      <router-link to="/videochat">
+      <router-link :to="{ name: 'VideochatMa' }">
         <v-btn>방매칭하기</v-btn>
       </router-link>
+      <router-link :to="{ name: 'VideochatShare' }">
+        <v-btn>방코드입장</v-btn>
+      </router-link>
+      <!-- <router-link to="/videochat">
+        <v-btn>방매칭하기</v-btn>
+      </router-link> -->
       <router-link :to="{ name: 'Planner' }">
         <v-btn>여행플래너</v-btn>
       </router-link>
@@ -37,77 +43,46 @@
         <router-link :to="{ name: 'MemberLogin' }">
           <v-btn>로그인</v-btn>
         </router-link>
-        <router-link :to="{ name: 'VideochatMa' }">
-          <v-btn>방매칭하기</v-btn>
+        <router-link :to="{ name: 'MemberRegister' }">
+          <v-btn>회원가입</v-btn>
         </router-link>
-        <router-link :to="{ name: 'VideochatShare' }">
-          <v-btn>방코드입장</v-btn>
-        </router-link>
-        <!-- <router-link to="/videochat">
-        <v-btn>방매칭하기</v-btn>
-      </router-link> -->
-        <router-link :to="{ name: 'Planner' }">
-          <v-btn>여행플래너</v-btn>
-        </router-link>
+      </div>
+      <div v-else>
         <v-menu open-on-hover style="z-index: 3500">
           <template v-slot:activator="{ props }">
-            <v-btn v-bind="props"> 커뮤니티 </v-btn>
+            <v-btn icon v-bind="props">
+              <v-badge color="red" dot>
+                <v-icon>mdi-bell</v-icon>
+              </v-badge>
+            </v-btn>
           </template>
           <v-list>
-            <v-list-item v-for="(item, index) in items_community" :key="index" :value="index">
+            <v-list-item v-for="(item, index) in items_new" :key="index" :value="index">
               <v-list-item-title @click="$router.push({ name: item.name })">
                 {{ item.title }}
               </v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
-        <v-spacer></v-spacer>
-        <!-- false시 로그인 상태, true시 비로그인 상태 -->
-        <div v-if="!isLoggedIn">
-          <router-link :to="{ name: 'MemberLogin' }">
-            <v-btn>로그인</v-btn>
-          </router-link>
-          <router-link :to="{ name: 'MemberRegister' }">
-            <v-btn>회원가입</v-btn>
-          </router-link>
-        </div>
-        <div v-else>
-          <v-menu open-on-hover style="z-index: 3500">
-            <template v-slot:activator="{ props }">
-              <v-btn icon v-bind="props">
-                <v-badge color="red" dot>
-                  <v-icon>mdi-bell</v-icon>
-                </v-badge>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item v-for="(item, index) in items_new" :key="index" :value="index">
-                <v-list-item-title @click="$router.push({ name: item.name })">
-                  {{ item.title }}
-                </v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-          <v-menu open-on-hover style="z-index: 3500">
-            <template v-slot:activator="{ props }">
-              <v-btn v-bind="props">
-                <div style="font-size: x-small">{{ title }}</div>
-                <div>{{ currentUser }}</div>
-                <v-icon>mdi-chevron-down</v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item v-for="(item, index) in items_user" :key="index" :value="index">
-                <v-list-item-title @click="$router.push({ name: `${item.name}` })">
-                  {{ item.title }}
-                </v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </div>
-      </v-toolbar>
-    </div>
-  </v-app-bar>
+        <v-menu open-on-hover style="z-index: 3500">
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props">
+              <div style="font-size: x-small">{{ title }}</div>
+              <div>{{ currentUser }}</div>
+              <v-icon>mdi-chevron-down</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in items_user" :key="index" :value="index">
+              <v-list-item-title @click="$router.push({ name: `${item.name}` })">
+                {{ item.title }}
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+    </v-toolbar>
+  </div>
 </template>
 
 <script>
@@ -124,6 +99,7 @@ export default {
     ],
     items_user: [
       { title: "마이페이지", name: "" },
+      { title: "칭호설정", name: "MemberSetTitle" },
       { title: "로그아웃", name: "MemberLogout" },
     ],
     items_new: [{ title: "[Q&A] 에 답변이 달렸습니다." }, { title: "[대전 마스터] 업적을 달성하셨습니다." }],
