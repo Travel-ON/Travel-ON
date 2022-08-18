cbfccca50c1ef584906412e7d4e5e564c126a303
JHPark <erict4f@gmail.com>
2022-08-19 02:28:21 +0900

    jaehyun_Fix: 칭호 설정 오류, 플래너 표시 오류 수정

--- frontend/src/components/planner/PlanHistoryDetail.vue
+++ frontend/src/components/planner/PlanHistoryDetail.vue
@@ -26,7 +26,9 @@
           {{ plan.visitedPlace }}
         </div>
         <div style="display: flex; margin: 0 20px; align-items: center">
-          <div style="margin-right: 5px; color: #f5c343; font-size: 16px">{{ plan.ratePoint.toFixed(1) }}</div>
+          <div style="margin-right: 5px; color: #f5c343; font-size: 16px">
+            {{ plan.ratePoint ? plan.ratePoint.toFixed(1) : "" }}
+          </div>
           <div v-if="plan.ratePoint">
             <v-rating
               v-model="plan.ratePoint"
