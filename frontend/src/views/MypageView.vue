d1341d7e1c876390d93d70f0a0b9b4ee65ec13e7
Kuuuna98 <yunaghgh@naver.com>
2022-08-18 22:20:51 +0900

    yuna_Fix: 비로그인 시 서비스 제한

--- frontend/src/views/MypageView.vue
+++ frontend/src/views/MypageView.vue
@@ -40,13 +40,31 @@ export default {
   },
   created() {
     if (!this.isLoggedIn) {
+      Swal.fire({
+        title: "로그인이 필요한 서비스입니다.",
+        text: "로그인 화면으로 이동할까요?",
+        icon: "warning",
+        showCancelButton: true,
+        buttons: true,
+        dangerMode: true,
+        confirmButtonText: "확인",
+        cancelButtonText: "취소",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.$router.push({
+            name: "MemberLogin",
+          });
+        } else {
+          this.$router.push({
+            name: "home",
+          });
+        }
+      });
+    } else {
       this.$router.push({
-        name: "home",
+        name: "MemberSetTitle",
       });
     }
-    this.$router.push({
-      name: "MemberSetTitle",
-    });
   },
   methods: {
     ...mapActions(["delete"]),
